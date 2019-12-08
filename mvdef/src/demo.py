28aac71bcc0133958648c830c2283dec40690133
Louis Maddox <louismmx@gmail.com>
2019-12-08 10:22:50 +0000

    Move transfer logic to its own file, run demo by importing this

--- mvdef/src/demo.py
+++ mvdef/src/demo.py
@@ -1,63 +1,19 @@
-from sys import path as syspath
-from src.ast_util import ast_parse
-from src.backup import backup
 from src.__env__ import example_dir
-from src.display import colour_str as colour
-from example.test.test_demo_program import test_report
+from src.transfer import parse_transfer
+from example.test.test_demo import test_report as demotest
 
-# TODO: Move parse_example to AST once logic is figured out for the demo
-def parse_example(src_p, dst_p, mvdefs, report=True, nochange=True):
-    """
-    If nochange is False, files will be changed in place (i.e. setting it
-    to False is equivalent to setting the edit parameter to True).
-      - Note: this was unimplemented...
-
-    This parameter is used as a sanity check to prevent wasted computation,
-    as if neither report is True nor nochange is False, there is nothing to do.
-    """
-    # Backs up source and target to a hidden file, restorable in case of error,
-    # and creating a hidden placeholder if the target doesn't exist yet
-    assert True in [report, not nochange], "Nothing to do"
-    assert backup(src_p, dry_run=nochange)
-    assert backup(dst_p, dry_run=nochange)
-    # Create edit agendas from the parsed AST of source and destination files
-    src_edits = ast_parse(src_p, mvdefs=mvdefs, report=report)
-    assert src_edits is not None, "The src file did not return a processed AST"
-    if report:
-        print(f"⇒ Functions moving from {colour('light_gray', src_p)}: {mvdefs}")
-    transfers = dict([["take", src_edits.get("move")], ["echo", src_edits.get("copy")]])
-    dst_edits = ast_parse(dst_p, transfers=transfers, report=report)
-    if dst_edits is None:
-        # There is no destination file (it will be created)
-        if report:
-            print(
-                f"⇒ Functions will move to {colour('light_gray', dst_p)}"
-                + " (it's being created from them)"
-            )
-    else:
-        if report:
-            print(f"⇒ Functions will move to {colour('light_gray', dst_p)}")
-    if nochange:
-        print("DRY RUN: No files have been modified, skipping tests.")
-    else:
-        try:
-            test_report()
-        except AssertionError as e:
-            print(f"!!! The demo broke the example !!!")
-            raise RuntimeError(e)
-    return src_edits, dst_edits
 
 def main(mvdefs, dry_run=True, report=True):
     print("--------------RUNNING src.demo⠶main()--------------")
     try:
-        test_report()
+        demotest()
     except AssertionError as e:
         raise RuntimeError("The tests do not pass for the example file.")
 
     # Step 1: declare src and dst .py file paths and back up the files
     src_p, dst_p = (example_dir / f"{n}.py" for n in ["demo_program", "new_file"])
-    src_parsed, dst_parsed = parse_example(
-        src_p, dst_p, mvdefs=mvdefs, report=report, nochange=dry_run
+    src_parsed, dst_parsed = parse_transfer(
+        src_p, dst_p, mvdefs=mvdefs, test_func=demotest, report=report, nochange=dry_run
     )
     print("------------------COMPLETE--------------------------")
     return
---
+++ mvdef/src/transfer.py
@@ -0,0 +1,60 @@
+from src.ast_util import ast_parse
+from src.backup import backup
+from src.display import colour_str as colour
+
+# TODO: Move parse_example to AST once logic is figured out for the demo
+def parse_transfer(src_p, dst_p, mvdefs, test_func=None, report=True, nochange=True):
+    """
+    Execute the transfer of function definitions and import statements, optionally
+    (if test_func is specified) also calls that afterwards to confirm functionality
+    remains intact.
+
+    If test_func is specified, it must only use AssertionError (i.e. you are free
+    to have the test_func call other functions, but it must catch any errors therein
+    and only raise errors from assert statements). This is to simplify this step.
+    My example would be to list one or more failing tests, then assert that this
+    list is None, else raise an AssertionError of these tests' definition names
+    (see example.test.test_demo⠶test_report for an example of such a function).
+
+    If nochange is False, files will be changed in place (i.e. setting it
+    to False is equivalent to setting the edit parameter to True).
+      - Note: this was unimplemented...
+
+    This parameter is used as a sanity check to prevent wasted computation,
+    as if neither report is True nor nochange is False, there is nothing to do.
+    """
+    # Backs up source and target to a hidden file, restorable in case of error,
+    # and creating a hidden placeholder if the target doesn't exist yet
+    assert True in [report, not nochange], "Nothing to do"
+    assert backup(src_p, dry_run=nochange)
+    assert backup(dst_p, dry_run=nochange)
+    # Create edit agendas from the parsed AST of source and destination files
+    src_edits = ast_parse(src_p, mvdefs=mvdefs, report=report)
+    assert src_edits is not None, "The src file did not return a processed AST"
+    if report:
+        print(f"⇒ Functions moving from {colour('light_gray', src_p)}: {mvdefs}")
+    transfers = dict([["take", src_edits.get("move")], ["echo", src_edits.get("copy")]])
+    dst_edits = ast_parse(dst_p, transfers=transfers, report=report)
+    if dst_edits is None:
+        # There is no destination file (it will be created)
+        if report:
+            print(
+                f"⇒ Functions will move to {colour('light_gray', dst_p)}"
+                + " (it's being created from them)"
+            )
+    else:
+        if report:
+            print(f"⇒ Functions will move to {colour('light_gray', dst_p)}")
+    if nochange:
+        print("DRY RUN: No files have been modified, skipping tests.")
+    elif test_func is None:
+        return src_edits, dst_edits
+    else:
+        try:
+            test_func.__call__()
+        except AssertionError as e:
+            # TODO: implement backup restore
+            print(f"! {test_func} failed, indicating changes made by mvdef broke the"
+                + "program (if backups used, mvdefs will now attempt to restore)")
+            raise RuntimeError(e)
+    return src_edits, dst_edits
