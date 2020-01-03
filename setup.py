8d09f141116d0bc35e5724463e3f94b02bf01026
Louis Maddox <louismmx@gmail.com>
2020-01-03 15:56:05 +0000

    PyPi bug, republish, bump v0.2.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
  
 setup(
     name="mvdef",
-    version="0.2.6",
+    version="0.2.7",
     author="Louis Maddox",
     author_email="louismmx@gmail.com",
     description="Package to move functions and their import statements between files",
