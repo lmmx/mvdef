9ac0aabc4484942378848624437ac8f27cb169ce
Louis Maddox <louismmx@gmail.com>
2022-10-28 00:19:25 +0100

    Replaced ugly direct params with neater indirect fixtures

--- tests/diff_test.py
+++ tests/diff_test.py
@@ -1,11 +1,11 @@
 from enum import Enum
 from pathlib import Path
 
-from pytest import mark
+from pytest import fixture, mark
 
 from mvdef.cli import cli
 
-from .io import write_files
+from .io import Write
 
 __all__ = ["test_create_named_tmp_files", "test_mvdef_simple_class_move"]
 
@@ -13,7 +13,7 @@ __all__ = ["test_create_named_tmp_files", "test_mvdef_simple_class_move"]
 @mark.parametrize("a_cat,b_cat", [("aaa", "bbb")])
 def test_create_named_tmp_files(tmp_path, a_cat, b_cat):
     a, b = ("a.txt", "b.txt")
-    write_files([a, b], [a_cat, b_cat], path=tmp_path, len_check=True)
+    Write([a, b], [a_cat, b_cat], path=tmp_path, len_check=True)
 
 
 def get_mvdef_diffs(a: Path, b: Path, **mvdef_cli_args) -> tuple[str, str]:
@@ -23,44 +23,53 @@ def get_mvdef_diffs(a: Path, b: Path, **mvdef_cli_args) -> tuple[str, str]:
 
 
 class FuncAndClsDefs(Enum):
-    def_foo_cls_A = "x = 1\n\ndef foo():\n    print(1)\n\nclass A:\n    pass\ny = 2\n"
-    def_bar = "def bar():\n    print(2)\na = 1\n"
+    fooA = "x = 1\n\ndef foo():\n    print(1)\n\nclass A:\n    pass\ny = 2\n"
+    bar = "def bar():\n    print(2)\na = 1\n"
 
 
 class SrcDiffs(Enum):
-    FooWithoutBar = (
-        "--- original/foo.py\n+++ fixed/foo.py\n@@ -3,6 +3,5 @@\n def foo():\n"
+    fooA2bar_A = (
+        "--- original/fooA.py\n+++ fixed/fooA.py\n@@ -3,6 +3,5 @@\n def foo():\n"
         "     print(1)\n \n-class A:\n-    pass\n+\n y = 2\n"
     )
 
 
 class DstDiffs(Enum):
-    FooWithoutBar = (
+    fooA2bar_A = (
         "--- original/bar.py\n+++ fixed/bar.py\n@@ -1,3 +1,7 @@\n def bar():\n"
         "     print(2)\n a = 1\n+\n+\n+class A:\n+    pass\n"
     )
 
 
+@fixture(scope="function")
+def src(request) -> tuple[str, str]:
+    return FuncAndClsDefs[request.param]
+
+
+dst = src  # Clone with another name
+
+
+@fixture(scope="function")
+def stored_diffs(request) -> tuple[str, str]:
+    return SrcDiffs[request.param].value, DstDiffs[request.param].value
+
+
 @mark.parametrize(
-    "mv,cls_defs",
+    "mv,cls_defs,stored_diffs",
     [
-        (["A"], True),
-        (["A", "A"], True),
-        # (["foo"], False),
-        # (["foo", "foo"], False),
+        (["A"], True, "fooA2bar_A"),
+        (["A", "A"], True, "fooA2bar_A"),
+        # (["foo"], False, "fooA2bar_foo"),
+        # (["foo", "foo"], False, "fooA2bar_foo"),
     ],
+    indirect=["stored_diffs"],
 )
-@mark.parametrize("foo", [FuncAndClsDefs.def_foo_cls_A.value])
-@mark.parametrize("bar", [FuncAndClsDefs.def_bar.value])
-@mark.parametrize("src_diff", [SrcDiffs.FooWithoutBar.value])
-@mark.parametrize("dst_diff", [DstDiffs.FooWithoutBar.value])
-def test_mvdef_simple_class_move(tmp_path, mv, cls_defs, foo, bar, src_diff, dst_diff):
+@mark.parametrize("src,dst", [("fooA", "bar")], indirect=True)
+def test_mvdef_simple_class_move(tmp_path, mv, cls_defs, src, dst, stored_diffs):
     """
     Test that a class 'A' is moved correctly, and that repeating it twice makes no
     difference to the result.
     """
-    names = ("foo.py", "bar.py")
-    foo_p, bar_p = write_files(names, [foo, bar], path=tmp_path, len_check=True)
-    foo_diff, bar_diff = get_mvdef_diffs(a=foo_p, b=bar_p, mv=mv, cls_defs=cls_defs)
-    assert foo_diff == src_diff
-    assert bar_diff == dst_diff
+    src_p, dst_p = Write.from_enums(src, dst, path=tmp_path, len_check=True).file_paths
+    computed_diffs = get_mvdef_diffs(a=src_p, b=dst_p, mv=mv, cls_defs=cls_defs)
+    assert computed_diffs == stored_diffs
--- tests/io.py
+++ tests/io.py
@@ -1,14 +1,40 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
 from pathlib import Path
 
 __all__ = ["write_files"]
 
 
-def write_files(names, contents, path, len_check=True) -> list[Path]:
-    files = [path / name for name in names]
-    for file, content in zip(files, contents):
-        file.write_text(content)
-        assert file.read_text() == content
-    files_in_dir = list(path.iterdir())
-    if len_check:
-        assert len(files_in_dir) == len(files)
-    return files
+@dataclass
+class Write:
+    names: tuple[str, ...]
+    contents: tuple[str, ...]
+    path: Path
+    len_check: bool = True
+
+    @property
+    def file_paths(self) -> tuple[Path, ...]:
+        return tuple(self.path / name for name in self.names)
+
+    def __post_init__(self) -> None:
+        for file, content in zip(self.file_paths, self.contents):
+            file.write_text(content)
+            assert file.read_text() == content
+        files_in_dir = list(self.path.iterdir())
+        if self.len_check:
+            assert len(files_in_dir) == len(self.names)
+        return
+
+    @classmethod
+    def from_enums(
+        cls,
+        *enums: tuple[Enum, ...],
+        path: Path,
+        len_check: bool = len_check,
+        suffix: str = ".py",
+    ) -> Write:
+        names = tuple(f"{e.name}{suffix}" for e in enums)
+        contents = tuple(e.value for e in enums)
+        return cls(names=names, contents=contents, path=path, len_check=len_check)
